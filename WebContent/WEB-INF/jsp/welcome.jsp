2d76618a077c7137f29a82871a8841a286814446
wiewioraseb <wiewiora.seb@gmail.com>
2015-02-27 19:01:21 +0100

    Testing.

--- WebContent/WEB-INF/jsp/welcome.jsp
+++ WebContent/WEB-INF/jsp/welcome.jsp
@@ -1,7 +1,6 @@
 <html>
 <head>
-<title>Spring MVC Tutorial by Crunchify - Hello World Spring MVC
-	Example</title>
+<title>Spring MVC Tutorial BEER BLOG</title>
 <style type="text/css">
 body {
 	background-image: url('http://crunchify.com/bg.png');
--- src/com/beerblog/controller/BeerBlogHello.java
+++ src/com/beerblog/controller/BeerBlogHello.java
@@ -18,7 +18,14 @@ public class BeerBlogHello {
 				+ "<h3>********** Hello World, Beer Blog ON THE WAY</h3>This message is comming from BeerBlogHello.java **********<br><br>";
 		return new ModelAndView("welcome", "message", message);
 	}
-}
+	/*
+	@RequestMapping("/indeksownia")
+	public String indeksownia(){
+		return "/WEB-INF/jsp/jakas-strona.jsp";
+	}
+	*/
+	
+} // END OF CLASS
 
 // source
 // http://crunchify.com/simplest-spring-mvc-hello-world-example-tutorial-spring-model-view-controller-tips/
@@ -29,3 +36,6 @@ public class BeerBlogHello {
 // Koles z Czech
 // https://www.youtube.com/watch?v=mH2jx2RCu7o
 // !!
+
+// changing the root for localhost/beer-blog-hiccup
+// http://stackoverflow.com/questions/2437465/java-how-to-change-context-root-of-a-dynamic-web-project-in-eclipse
